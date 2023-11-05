18d9f1b4c7d8344ecebbf66dc09e2fe812076bfe
Ajay B <133206077+Ajaybalajiprasad@users.noreply.github.com>
2023-11-05 13:08:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const No = document.querySelector('.no');
+const Yes = document.querySelector('.yes');
+ Yes.addEventListener('click', () => {
+    question.innerHTML = "Yes! you are an Idiot :)";
+});
+No.addEventListener('mouseover', () => {
+    const wrapperRect = wrapper.getBoundingClientRect();
+    const NoRect = No.getBoundingClientRect();
+    const i = Math.floor(Math.random() * (wrapperRect.width - NoRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - NoRect.height)) + 1
+    No.style.left = i + "px";
+    No.style.top = j + "px";
+});
